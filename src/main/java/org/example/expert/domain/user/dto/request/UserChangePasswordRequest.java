0dcd4dbc9475fd47bf354e4e8f22c320e5a19dd0
sseung0510 <choiseunghi0510@gmail.com>
2025-12-03 17:02:27 +0900

    lv2-3: validation
    1. 요청 DTO에 비밀번호 조건 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "예전 비밀번호를 입력해주세요.")
     private String oldPassword;
-    @NotBlank
+
+    @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$" , message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
